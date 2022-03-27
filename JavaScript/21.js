62ea4949085ae96972ae157994c8bd018e038af7
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-27 21:48:07 +0530

    while loop

--- JavaScript/21.js
+++ JavaScript/21.js
@@ -4,13 +4,13 @@ let num = 100;
 // let i = 0;
 
 
-// while(i<=100){
-//     total = total + i;
-//     i++;
-// }
+while(i<=100){
+    total = total + i;
+    i++;
+}
 
 
-// console.log(total);
+console.log(total);
 
-// let total = (num*(num+1))/2;
-// console.log(total);
+let total = (num*(num+1))/2;
+console.log(total);
