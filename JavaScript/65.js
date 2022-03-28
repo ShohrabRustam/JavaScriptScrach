0a868a9d78215e3303725bf96a3806dc5d1ebf67
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-28 22:16:49 +0530

    splice  method

--- JavaScript/65.js
+++ JavaScript/65.js
@@ -7,7 +7,7 @@ const myArray = ['item1', 'item2', 'item3'];
 const deletedItems = myArray.splice(1, 2);
 console.log("delted item", deletedItems);
 // insert 
-// myArray.splice(1, 0,'inserted item');
+myArray.splice(1, 0,'inserted item');
 
 // insert and delete 
 const deletedItem = myArray.splice(1, 2, "inserted item1", "inserted item2")
