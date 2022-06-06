14fec71c4ae4fa0af32144435a7c21d961344e24
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-06 17:01:36 +0530

    getting the inderstandingof the promises

--- JS/promises.js
+++ JS/promises.js
@@ -5,17 +5,45 @@ let stocks = {
     toppings: ['chocolate', 'peanuts']
 }
 
-let is_shop_open= true;
+let is_shop_open = true;
 
-let order =(time,work)=>{
-    return new Promise((resolve,reject)=>{
-        if (is_shop_open){
-            setTimeout(()=>{
+let order = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
+            setTimeout(() => {
                 resolve(work());
-            },time)
-        }else{
+            }, time)
+        } else {
             reject(console.log('Our Shop is closed'));
         }
     })
 }
-order(2000,() => console.log(`${stocks.Fruits[0]}`));
+order(2000, () => console.log(`${stocks.Fruits[0]}` + ` was selected from stocks`))
+
+    .then(() => {
+        return order(0000, () => console.log('Production has started'))
+    })
+
+    .then(() => {
+        return order(2000, () => console.log(`${stocks.Fruits[0]}` + ` Fruit was chopped`))
+    })
+    .then(() => {
+        return order(1000, () => console.log(`Add ` + `${stocks.Liquids[0]}` + ' and ' + `${stocks.Liquids[1]}`))
+    })
+
+    .then(() => {
+        return order(1000, () => console.log('Start the Machine'))
+    })
+    .then(() => {
+        return order(2000, () => console.log(`Ice cream placed on ` + `${stocks.holder[0]} `))
+    })
+    .then(() => {
+        return order(3000, () => console.log(`${stocks.toppings[0]} was added as toppings `))
+    })
+    .then(() => {
+        return order(2000, () => console.log(`Serve Ice Cream !!! `))
+    })
+
+    .catch(()=>{
+        console.log("Customer Left !"); 
+    })
