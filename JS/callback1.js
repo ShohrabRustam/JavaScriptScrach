1d5c41c522980429813e1374ac21bc9720c7abf5
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-06 13:35:07 +0530

    under standing of the callback function in js

--- JS/callback1.js
+++ JS/callback1.js
@@ -1,12 +1,35 @@
-let order = (call_production) => {
-    console.log("Order Placed , Please Call Production ");
+let stocks={
+    Fruits:['strawberry', 'grapes','banana','mango','apple'],
+    Liquids:['water','ice'],
+    holder:['cone','cup','stick'],
+    toppings:['chocolate','peanuts']
+
+}
+// console.log("Print Banana from Stocks :  "+stocks.Fruits[2])
+// console.log("Print all Key of Stocks : "+ Object.keys(stocks))
+// let step1order = (Fruit_name,call_production) => {
+//     console.log("Order Placed , Please Call Production ");
+//     setTimeout(() => {
+//         call_production();
+//     }, 3000);
+// }
+
+// let step2produnction = () => {
+//     console.log("Order Received , Start Production ");
+// };
+
+// step1order(step2produnction);
+
+let order = (Fruit_name,call_production)=>{
     setTimeout(() => {
+        console.log(`${stocks.Fruits[Fruit_name]} was selected from stocks`)
         call_production();
-    }, 3000);
+    },2000)
+}
+let production=()=>{
+    setTimeout(()=>{
+        console.log("Production has started ");
+    },0000)
 }
 
-let produnction = () => {
-    console.log("Order Received , Start Production ");
-};
-
-order(produnction);
+order(0,production)
