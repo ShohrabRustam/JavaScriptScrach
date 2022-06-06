0acb69b3f85628fbf052290ab928cbcfec37c9e5
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-06 15:37:58 +0530

    promises in js

---
+++ JS/promises.js
@@ -0,0 +1,20 @@
+let stocks = {
+    Fruits: ['strawberry', 'grapes', 'banana', 'mango', 'apple'],
+    Liquids: ['water', 'ice'],
+    holder: ['cone', 'cup', 'stick'],
+    toppings: ['chocolate', 'peanuts']
+}
+
+let is_shop_open= true;
+
+let order =(time,work)=>{
+    return new Promise((resolve,reject)=>{
+        if (is_shop_open){
+            setTimeout(()=>{
+                resolve(work());
+            },time)
+        }else{
+            reject(console.log('Our Shop is closed'));
+        }
+    })
+}
