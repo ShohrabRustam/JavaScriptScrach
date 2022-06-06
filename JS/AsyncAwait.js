1cbb271f35f302b5e5fb8348c7fa0ae3fe72276a
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-06 17:22:02 +0530

    await in js

---
+++ JS/AsyncAwait.js
@@ -0,0 +1,31 @@
+let stocks = {
+    Fruits: ['strawberry', 'grapes', 'banana', 'mango', 'apple'],
+    Liquids: ['water', 'ice'],
+    holder: ['cone', 'cup', 'stick'],
+    toppings: ['chocolate', 'peanuts']
+}
+
+let is_shop_open = true;
+
+// let order = () => {
+//     return new Promise((resolve, reject) => {
+//         if (is_shop_open) {
+//             resolve()
+//         } else {
+//             reject()
+//         }
+//     })
+// }
+
+async function order(){
+try{
+await abc;
+}
+catch(error){
+console.log("Abc doesn't exist ",error);
+}
+finally{
+    console.log("Run code Any way!");
+}
+}
+order();
