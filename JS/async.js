4f3ecd63e57b6048f542c794effd1133a705ca1e
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-06 12:16:11 +0530

    understanding of the synchronous and asynchronous

---
+++ JS/async.js
@@ -0,0 +1,38 @@
+
+    setTimeout(()=>{
+        console.log(' I ')
+    },1500)
+
+setTimeout(()=>{
+    console.log(' will ')
+},3000)
+
+
+setTimeout(() => {
+    console.log(' Kill ')
+}, 4500)
+
+setTimeout(()=>{
+    console.log(' You ')
+},6000)
+
+setTimeout(()=>{
+    console.log(' after ')
+},7500)
+
+setTimeout(()=>{
+    console.log(' next ')
+},9000)
+
+setTimeout(()=>{
+    console.log(' 5 minutes ')
+},10500)
+
+setTimeout(()=>{
+    console.log('So, be alert ')
+},12000)
+setTimeout(()=>{
+    console.log(' Kaisa Laga Mere Mazak !! , I just Kidding !!! ')
+},15000)
+
+console.log('Everything come after 1 seconds')
